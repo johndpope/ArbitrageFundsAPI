f4a75b84b731ffcae8572bd67e276d19250780c8
kgorde <37445671+kgorde@users.noreply.github.com>
2019-06-17 17:03:08 -0400

    Update settings.py
    Allow CORS

--- ArbitrageFundsAPI/settings.py
+++ ArbitrageFundsAPI/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = '../static'
+CORS_ORIGIN_ALLOW_ALL = True 
